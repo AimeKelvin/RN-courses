4b10ce6c5fc89eba2cde521b669ea30de8c0c175
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-11-11 11:20:42 +0200

    init commit, setup react native and nativewind

---
+++ map-app/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-blue-200 min-h-screen"
+    >
+      <Text className="text-4xl font-bold text-white">Eat pussy</Text>
+    </View>
+  );
+}
